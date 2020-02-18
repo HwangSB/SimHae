2e8605737e2db3f700300a3824e316b1ddf52fe8
HwangSB <hsb990105@gmail.com>
2020-02-18 16:45:42 +0900

    sb test

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Solution Challeng',
+      title: 'Solution Challenge',
       theme: ThemeData(
         brightness: Brightness.light,
       ),
